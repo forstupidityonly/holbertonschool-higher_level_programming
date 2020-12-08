fa84eefb78518e531050019ad02d6bcdff0a7fd6
Corbin <forstupidityonly@gmail.com>
2020-12-08 12:04:17 -0600

    : throw me off

---
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+import random
+number = random.randint(-10000, 10000)
+digit = number % 10 if number > 0 else number % -10
+print("Last digit of {} is {} and is ".format(number, digit), end='')
+if digit > 5:
+    print("is greater than 5")
+elif digit == 0:
+    print("is 0")
+else:
+    print("is less than 6 and not 0")
