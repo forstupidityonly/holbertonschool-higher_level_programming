ea09d0f6892dfd4991604a86c23133e2ee4205fb
Corbin <forstupidityonly@gmail.com>
2020-12-08 16:25:55 -0600

    lol no

---
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -0,0 +1,14 @@
+#include "lists.h"
+/**
+  *
+  *
+  */
+listint_t *insert_node(listint_t **head, int number)
+{
+	listint_t *current, *last, *new;
+
+	if (head == NULL)
+		return (NULL);
+	last = head;
+	if (last == NULL || number < ['/j
+}
