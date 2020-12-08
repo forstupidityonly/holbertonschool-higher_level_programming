279fa80df53ef7ad35235ce987a07fdeb0bbe9e8
Corbin <forstupidityonly@gmail.com>
2020-12-08 16:27:29 -0600

    hi effencie

--- 0x01-python-if_else_loops_functions/13-insert_number.c
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -5,10 +5,6 @@
   */
 listint_t *insert_node(listint_t **head, int number)
 {
-	listint_t *current, *last, *new;
-
-	if (head == NULL)
-		return (NULL);
-	last = head;
-	if (last == NULL || number < ['/j
+	head = void;
+	number = void;
 }
