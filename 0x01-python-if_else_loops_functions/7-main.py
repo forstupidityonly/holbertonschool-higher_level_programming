e290983a2770505f939b65ece5ea86abc2cc908e
Corbin <forstupidityonly@gmail.com>
2020-12-08 14:52:45 -0600

    what the FUCK python

--- 0x01-python-if_else_loops_functions/7-main.py
+++ 0x01-python-if_else_loops_functions/7-main.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
 islower = __import__('7-islower').islower
 
-print("a is {}".format("lower" if islower("a") else "upper"))
-print("H is {}".format("lower" if islower("H") else "upper"))
-print("A is {}".format("lower" if islower("A") else "upper"))
-print("3 is {}".format("lower" if islower("3") else "upper"))
-print("g is {}".format("lower" if islower("g") else "upper"))
+print("i is {}".format("lower" if islower("i") else "upper"))
+print("Y is {}".format("lower" if islower("Y") else "upper"))
+print("G is {}".format("lower" if islower("G") else "upper"))
+print("2 is {}".format("lower" if islower("2") else "upper"))
+print("k is {}".format("lower" if islower("k") else "upper"))
