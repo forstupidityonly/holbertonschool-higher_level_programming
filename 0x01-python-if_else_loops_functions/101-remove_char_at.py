251789e8c0f77b2c0ae8ca2b5c4cea8c64170656
Corbin <forstupidityonly@gmail.com>
2020-12-08 17:44:58 -0600

    pep

--- 0x01-python-if_else_loops_functions/101-remove_char_at.py
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def remove_char_at(str, n):
     result = ""
-    for i in range(0, len(str)): 
-    if i != n: 
-         result = result + str[i]
+    for i in range(0, len(str)):
+        if i != n:
+            result = result + str[i]
     return (result)
