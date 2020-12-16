c7a3514ee05dcbecb6556a318da7d89309d39fa8
Corbin <forstupidityonly@gmail.com>
2020-12-16 10:37:13 -0600

    my_list of i not just i

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -4,7 +4,7 @@ def divisible_by_2(my_list=[]):
         return None
     tmp = []
     for i in range(len(my_list)):
-        if i % 2 == 0:
+        if my_list[i] % 2 == 0:
             tmp.append(True)
         else:
             tmp.append(False)
