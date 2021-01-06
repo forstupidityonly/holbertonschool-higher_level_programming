d505094ac4c88b017be34db099db5bb86601dac0
Corbin <forstupidityonly@gmail.com>
2021-01-05 18:24:21 -0600

    compile

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -1,17 +1,23 @@
 #!/usr/bin/pyhton3
 """my square mod"""
+
+
 class Square:
     """deff the square"""
+
     def __init__(self, size=0):
+        """make it
+           args size of sueare
+           raise int and pos or 0
+        """
         if not isinstance(size, int):
-            """make it 
-               args size of sueare
-               raise int and pos or 0
-            """
             raise TypeError("size must be an integer")
         if size < 0:
             raise ValueError("size must be >= 0")
         self.__size = size
-"""area of square"""
+
+    """area of square"""
+
     def area(self):
-        return self.__size ** 2
+        """area"""
+        return self.__size * self.__size
