40a266c677f479720ec1d659e779c5cc2493f0cd
Corbin <forstupidityonly@gmail.com>
2021-03-01 15:41:00 -0600

    lol this actually looks like a script best wors

--- 0x0D-SQL_introduction/7-insert_value.sql
+++ 0x0D-SQL_introduction/7-insert_value.sql
@@ -1,6 +1,9 @@
 -- Write a script that inserts a new row in the table 
 -- first_table (database hbtn_0c_0) in your MySQL server.
-INSERT INTO hbtn_0c_0.first_table (
-    id=89,
-    name=Holberton School
+INSERT INTO first_table (
+    id,
+    name
+) VALUES (
+    89,
+    'Holberton School'
 );
