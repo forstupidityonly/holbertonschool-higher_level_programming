230ff6eba5388745188c95b53e124eb62d7ab634
Corbin <forstupidityonly@gmail.com>
2021-03-02 14:26:09 -0600

    two commits?

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,3 +1,4 @@
 -- lists all privileges of user_0d_1 and user_0d_2
+-- apperently there has to be two fucking commmit lines
 SHOW GRANTS FOR user_0d_1@localhost;
 SHOW GRANTS FOR user_0d_2@localhost;
