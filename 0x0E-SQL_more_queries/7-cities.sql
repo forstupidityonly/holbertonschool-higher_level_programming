ec89914d7a7f0905edbb043f55d70f07fb9fa613
Corbin <forstupidityonly@gmail.com>
2021-03-02 17:26:20 -0600

    gotta name it right

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -4,7 +4,7 @@
 -- name VARCHAR(256) can’t be null DO NOT FAIL
 
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
-CREATE TABLE IF NOT EXISTS hbtn_0d_usa.states (
+CREATE TABLE IF NOT EXISTS hbtn_0d_usa.cities (
     id INT UNIQUE NOT NULL AUTO_INCREMENT PRIMARY KEY,
     state_id INT NOT NULL,
     name VARCHAR(256) NOT NULL,
