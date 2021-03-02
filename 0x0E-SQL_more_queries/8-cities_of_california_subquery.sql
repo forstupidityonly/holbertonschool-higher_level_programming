4f6f26f2e97913ace8ceca85132cea273ab7cae2
Corbin <forstupidityonly@gmail.com>
2021-03-02 17:42:54 -0600

    can these be nested?

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,4 +1,4 @@
 -- lists all cities of California in hbtn_0d_usa. states table contains only
 -- one record where name = California (but the id can be different, as per the example)
 -- sorted in ascending order by cities.id no JOIN
-SELECT id, name FROM cities WHERE state_id = (SELECT FROM states WHERE id = 'California') ORDER BY id ASC;
+SELECT id, name FROM cities WHERE state_id = (SELECT id FROM states WHERE name = 'California') ORDER BY id ASC;
