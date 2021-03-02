a5785495cba694db5346ae2200008d0619318b8f
Corbin <forstupidityonly@gmail.com>
2021-03-02 17:36:06 -0600

    wtf

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,6 +1,4 @@
 -- lists all cities of California in hbtn_0d_usa. states table contains only
 -- one record where name = California (but the id can be different, as per the example)
 -- sorted in ascending order by cities.id no JOIN
-SELECT id, state_id, name
-FROM hbtn_0d_usa
-WHERE name = "California";
+SELECT id, name FROM cities WHERE state_id = ('California') ORDER BY id ASC;
