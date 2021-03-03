289396835d0b159e4b293376d9403f59fe481b9e
Corbin <forstupidityonly@gmail.com>
2021-03-02 20:01:47 -0600

    if this doesnt work

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -4,7 +4,7 @@
 -- doesn’t have any shows linked descending order by the number of shows linked
 SELECT tv_genres.name AS "genre", count(*) AS "number_of_shows"
 FROM tv_show_genres
-LEFT JOIN tv_show_genres
-ON tv_shows.genre_id = genre_id
+LEFT JOIN tv_genres
+ON tv_genres.id = genre_id
 GROUP BY genre_id
 ORDER BY count(*) DESC;
