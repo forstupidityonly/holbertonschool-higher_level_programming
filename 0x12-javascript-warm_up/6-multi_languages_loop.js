99cb2e0c72b6750bd6015877fdb97f031691e76b
Corbin <forstupidityonly@gmail.com>
2021-03-22 15:26:25 -0500

    lmao js formatting?

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 const arrayString = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
-asLen = arrayString.length;
-for (i = 0; i < asLen; i++) {
+const asLen = arrayString.length;
+for (let i = 0; i < asLen; i++) {
   console.log(arrayString[i]);
 }
