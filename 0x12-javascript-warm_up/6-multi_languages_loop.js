a96a6d8b23f8973e350c76254ded909df7ba85c1
Corbin <forstupidityonly@gmail.com>
2021-03-22 15:23:39 -0500

    hope i dont need to define my vars

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,6 @@
+#!/usr/bin/node
+const arrayString = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+asLen = arrayString.length;
+for (i = 0; i < asLen; i++) {
+  console.log(arrayString[i]);
+}
