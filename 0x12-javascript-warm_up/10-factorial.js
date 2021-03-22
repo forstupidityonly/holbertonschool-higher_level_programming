457d78c6dd40eaf426cf59ce7d8747bb2bb0e251
Corbin <forstupidityonly@gmail.com>
2021-03-22 17:07:18 -0500

    https://www.natashatherobot.com/factorials-javascript-recursion/

---
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+const num = parseInt(process.argv[2]);
+if (isNaN(num)) {
+  console.log(1);
+} else {
+  console.log(factorial(num));
+}
+
+function factorial (num) {
+  if (num === 0) {
+    return (1);
+  } else {
+    return (num * factorial(num - 1));
+  }
+}
