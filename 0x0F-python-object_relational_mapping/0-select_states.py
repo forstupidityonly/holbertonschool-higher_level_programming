b1afec2f4241ba60e438739af56a843767a0fece
Corbin <forstupidityonly@gmail.com>
2021-03-27 11:34:36 -0500

    pep8error

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -7,16 +7,15 @@ from sys import argv
 if __name__ == "__main__":
     db = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
                          passwd=argv[2], db=argv[3])
-
     """
-        cursor gives ability to have multiple seperate working environments through
-        the same connection to the database
+        cursor gives ability to have multiple seperate working environments
+        through the same connection to the database
     """
     cur = db.cursor()
     cur.execute("SELECT * FROM states ORDER BY states.id ASC")
     rows = cur.fetchall()
     for row in rows:
-        print (row)
+        print(row)
         """
             close all open cursors, and close all open database connections
         """
