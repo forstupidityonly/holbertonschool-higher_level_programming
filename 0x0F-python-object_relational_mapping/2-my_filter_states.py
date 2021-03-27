3f6155c2217ec2b2c1dd7f35537616cde6776267
Corbin <forstupidityonly@gmail.com>
2021-03-27 11:39:14 -0500

    taberror and pep8

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -9,17 +9,18 @@ if __name__ == "__main__":
                          passwd=argv[2], db=argv[3])
 
     """
-        cursor gives ability to have multiple seperate working environments through
-        the same connection to the database
+        cursor gives ability to have multiple seperate working environments
+        through the same connection to the database
     """
-cur = db.cursor()
-cur.execute(
-    "SELECT * FROM states WHERE name='{:s}' ORDER BY states.id ASC".format(argv[4]))
-rows = cur.fetchall()
-for row in rows:
-    print (row)
-    """
-        close all open cursors, and close all open database connections
-    """
-cur.close()
-db.close()
+    cur = db.cursor()
+    cur.execute(
+        "SELECT * FROM states WHERE name='{:s}' ORDER BY states.id ASC".format(
+            argv[4]))
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
+        """
+            close all open cursors, and close all open database connections
+        """
+    cur.close()
+    db.close()
--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -11,11 +11,11 @@ if __name__ == "__main__":
         user=argv[1],
         passwd=argv[2],
         db=argv[3])
-cur = db.cursor()
-cur.execute("""SELECT cities.id, cities.name, states.name FROM cities
-    INNER JOIN states ON cities.state_id = states.id ORDER BY cities.id ASC""")
-rows = cur.fetchall()
-for row in rows:
-    print (row)
-cur.close()
-db.close()
+    cur = db.cursor()
+    cur.execute("""SELECT cities.id, cities.name, states.name FROM cities
+        INNER JOIN states ON cities.state_id = states.id ORDER BY cities.id ASC""")
+    rows = cur.fetchall()
+    for row in rows:
+        print (row)
+    cur.close()
+    db.close()
