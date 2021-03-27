e33771ffaccb4633e51c5e86a72c4fc89d6fd211
Corbin <forstupidityonly@gmail.com>
2021-03-27 15:03:46 -0500

    stuff and things

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -1,10 +1,17 @@
 #!/usr/bin/python3
+    """[summary]
+    """
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
 
 class State(Base):
+    """[summary]
+
+    Args:
+        Base ([type]): [description]
+    """
     __tablename__ = 'states'
     id = Column(Integer, primary_key=True, nullable=False)
     name = Column(String(128), nullable=False)
