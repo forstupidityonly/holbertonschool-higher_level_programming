b9100c03ce3f4724f250fea992dfbe4b22d89da8
Corbin <forstupidityonly@gmail.com>
2021-03-28 15:32:56 -0500

    simplified

--- 0x0F-python-object_relational_mapping/11-model_state_insert.py
+++ 0x0F-python-object_relational_mapping/11-model_state_insert.py
@@ -13,7 +13,8 @@ if __name__ == "__main__":
     engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
         argv[1], argv[2], argv[3]), pool_pre_ping=True)
     Base.metadata.create_all(engine)
-    session = sessionmaker(bind=engine)()
+    Session = sessionmaker(bind=engine)
+    session = Session()
     state = State("Louisiana")
     session.add(state)
     session.commit()
