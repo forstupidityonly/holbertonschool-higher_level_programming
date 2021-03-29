5b0a1ba5187c34d7d49a62a0d944a9a6b8addd42
Corbin <forstupidityonly@gmail.com>
2021-03-28 22:43:14 -0500

    nope

--- 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -15,7 +15,7 @@ if __name__ == "__main__":
     Base.metadata.create_all(engine)
     Session = sessionmaker(bind=engine)
     session = Session()
-    state = session.query(State).filter(State).filter_by(id=2).first()
+    state = session.query(State).filter_by(id=2).first()
     state.name = "New Mexico"
     session.commit()
     session.close()
