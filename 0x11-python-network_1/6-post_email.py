9a6436ba005c37dbefbe77be241b48308a176c1a
Corbin <forstupidityonly@gmail.com>
2021-04-09 18:56:09 -0500

    im so lost

---
+++ 0x11-python-network_1/6-post_email.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""comment"""
+from sys import argv
+import requests
+
+
+if __name__ == "__main__":
+    if argv[1] is not None and argv[2] is not None:
+        url = argv[1]
+        email = argv[2]
+        data = {'email': email}
+        info = requests.cost(url, data)
+        value = info.text
+        print(str(value))
