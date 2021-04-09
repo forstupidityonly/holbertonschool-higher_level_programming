2084f28a3357e01be6b3f747a8679554a09c5dd2
Corbin <forstupidityonly@gmail.com>
2021-04-09 15:18:43 -0500

    here is to hopeing

---
+++ 0x11-python-network_1/2-post_email.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""Python script that takes in a URL and an email, sends a POST request to the
+passed URL with the email as a parameter, and displays the body of the
+response (decoded in utf-8)"""
+import urllib.request
+import urllib.parse
+from sys import argv
+
+
+if __name__ == "__main__":
+    url = argv[1]
+    email = argv[2]
+    values = {"email": email}
+    data = urllib.parse.urlencode(values)
+    data = query_string.encode("ascii")
+
+    with urllib.request.urlopen(url, data) as response:
+    response_text = response.read()
+    response_text = response_text.decode("UTF-8")
+    print(response_text)
