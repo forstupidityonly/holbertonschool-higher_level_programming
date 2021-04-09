dce8b68ab3e1afd2d2ff2ec84ab4bd80dc5368f1
Corbin <forstupidityonly@gmail.com>
2021-04-09 16:26:54 -0500

    error

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -6,11 +6,10 @@ from sys import argv
 
 
 if __name__ == "__main__":
-    if argv[1] is not None:
-        try:
-            with urllib.request.urlopen(argv[1]) as response:
-                body = response.read()
-                body = body.decode("UTF-8")
-                print(body)
-        except urllib.error.HTTPError as e:
-            print("Error code: {}".format(e.code))
+    try:
+        with urllib.request.urlopen(argv[1]) as response:
+            body = response.read()
+            body = body.decode("UTF-8")
+            print(body)
+    except urllib.error.HTTPError as e:
+        print("Error code: {}".format(e.code))
