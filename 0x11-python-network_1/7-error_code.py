26a37b4256655579798870dce4e5bb09aab448f0
Corbin <forstupidityonly@gmail.com>
2021-04-09 19:32:56 -0500

    :

---
+++ 0x11-python-network_1/7-error_code.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""comment"""
+import requests
+from sys import argv
+
+
+if __name__ == '__main__':
+    r = requests.get(argv[1])
+    status = r.status_code
+    if status >= 400:
+        print("Error cde: {}".format(status))
+    else:
+        print(r.text)
