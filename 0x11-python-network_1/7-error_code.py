c3040abd4a5cc562bf3b73c018ccb9d78fce11d5
Corbin <forstupidityonly@gmail.com>
2021-04-09 19:35:39 -0500

    spelling

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -8,6 +8,6 @@ if __name__ == '__main__':
     r = requests.get(argv[1])
     status = r.status_code
     if status >= 400:
-        print("Error cde: {}".format(status))
+        print("Error code: {}".format(status))
     else:
         print(r.text)
