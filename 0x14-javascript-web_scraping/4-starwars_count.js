9355643c5964aed7688c466dfc232008d1b1721e
Corbin <forstupidityonly@gmail.com>
2021-05-28 03:25:40 -0500

    i hate java

---
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -0,0 +1,20 @@
+#!/usr/bin/node
+
+const request = require('request');
+request.get(process.argv[2], function (err, r, body) {
+  if (err) {
+  console.log(err);
+  } else {
+    let Count = 0;
+    const movies = JSON.parse(body).results;
+    for (let x = 0; x < movies.length; x++) {
+      for (let y = 0; y < movies[x].characters.length; y++) {
+        if (movies[x].characters[y].includes('/18/')) {
+          Count++;
+          break;
+        }
+      }
+    }
+    console.log(Count);
+  }
+});
