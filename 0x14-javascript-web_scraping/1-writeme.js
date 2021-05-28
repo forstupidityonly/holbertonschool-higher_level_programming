5c3b3a1bebdc7247fe9aafb39fdd7d760a6f6d18
Corbin <forstupidityonly@gmail.com>
2021-05-28 02:43:59 -0500

    test

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -2,6 +2,6 @@
 
 const fs = require('fs');
 const myArgs = process.argv.slice(2);
-fs.writeFile(myArgs[0], muArgs[1], 'utf-8', (err, data) => {
+fs.writeFile(myArgs[0], myArgs[1], 'utf-8', (err, data) => {
   if (err) console.log(err);
 });
