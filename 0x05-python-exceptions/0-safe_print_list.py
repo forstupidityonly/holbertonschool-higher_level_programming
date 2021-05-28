7dce95fb28f4c563d5872f194774ddfa1894b962
Corbin <forstupidityonly@gmail.com>
2021-05-28 00:08:47 -0500

    old projects seem like a better use of my time

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,3 +1,11 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
-    
+    count = 0
+    try:
+        while count is not x:
+            print(my_list[items], end'')
+            count = count + 1
+    except IndexError:
+        None
+    print()
+    return count
