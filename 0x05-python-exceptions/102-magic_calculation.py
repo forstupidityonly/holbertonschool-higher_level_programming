2bf9f7f3f3e500269ddd77d55628b8004c4ce056
Corbin <forstupidityonly@gmail.com>
2021-05-28 02:15:46 -0500

    pep8

--- 0x05-python-exceptions/102-magic_calculation.py
+++ 0x05-python-exceptions/102-magic_calculation.py
@@ -7,7 +7,7 @@ def magic_calculation(a, b):
                 raise Exception('Too far')
             else:
                 res = ((a ** b) / i) + res
-        except:
-           res = b + a
-           break
+        except BaseException:
+            res = b + a
+            break
     return res
