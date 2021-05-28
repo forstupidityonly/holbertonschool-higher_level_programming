3510fc524ef4b2cc5507fff587b6a676fc89547b
Corbin <forstupidityonly@gmail.com>
2021-05-28 03:26:27 -0500

    fix

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -3,7 +3,7 @@
 const request = require('request');
 request.get(process.argv[2], function (err, r, body) {
   if (err) {
-  console.log(err);
+    console.log(err);
   } else {
     let Count = 0;
     const movies = JSON.parse(body).results;
