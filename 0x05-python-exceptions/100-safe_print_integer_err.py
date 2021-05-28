5984da60672fd2f8177649481f460ef05d13af20
Corbin <forstupidityonly@gmail.com>
2021-05-28 02:01:08 -0500

    oh well

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -6,6 +6,6 @@ def safe_print_integer_err(value):
     try:
         print("{:d}".format(value))
         return True
-    except Exception as code:
-        print("Exception: {}".format(code), file=sys.stderr)
+    except Exception as val:
+        sys.stderr.write("Exception: {}\n".format(val))
         return False
