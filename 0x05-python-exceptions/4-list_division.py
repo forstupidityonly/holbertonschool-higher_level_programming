919f3c2f01ee4f36acd7ce3179feeaec584d901c
Corbin <forstupidityonly@gmail.com>
2021-05-28 01:37:12 -0500

    spelling

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -7,7 +7,7 @@ def list_division(my_list_1, my_list_2, list_length):
         try:
             aws = my_list_1[idx] / my_list_2[idx]
         except ZeroDivisionError:
-            print("dicision by 0")
+            print("division by 0")
         except IndexError:
             print("out of range")
         except TypeError:
