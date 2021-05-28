838d5ea79e3a41fa577619503d6519750728a509
Corbin <forstupidityonly@gmail.com>
2021-05-28 02:26:07 -0500

    first push

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,7 +1,8 @@
 #!/usr/bin/node
 //reads a file
 const fs = require('fs');
-fs.readFile()process.argv[2], 'utf8', (error, data) => {
-    if (err) console.log(err);
-    else console.log(data);
+const myArgs = process.argv.slice(2);
+fs.readFile(myArgs[0], 'utf-8', (err, data) => {
+    if (err) { console.log(err); }
+    console.log(data);
 });
