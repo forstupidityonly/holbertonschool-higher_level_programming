e391260de03658ca9b6f3aad74e91d9ebbda2a2b
Corbin <forstupidityonly@gmail.com>
2021-05-28 02:37:06 -0500

    style?

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -3,6 +3,6 @@
 const fs = require('fs');
 const myArgs = process.argv.slice(2);
 fs.readFile(myArgs[0], 'utf-8', (err, data) => {
-    if (err) { console.log(err); }
-    console.log(data);
+  if (err) { console.log(err); }
+  console.log(data);
 });
