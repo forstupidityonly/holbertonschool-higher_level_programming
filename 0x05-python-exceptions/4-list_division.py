a4cf04d3edeff406f95603724bcf97859a13d301
Corbin <forstupidityonly@gmail.com>
2021-05-28 01:12:11 -0500

    that was pretty good

---
+++ 0x05-python-exceptions/4-list_division.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+def list_division(my_list_1, my_list_2, list_length):
+    new_list = []
+    idx = 0
+    while idx < list_length:
+        aws = 0
+        try:
+            aws = my_list_1[idx] / my_list_2[idx]
+        except ZeroDivisionError:
+            print("dicision by 0")
+        except IndexError:
+            print("out of range")
+        except TypeError:
+            print("wrong type")
+        finally:
+            new_list.append(aws)
+            idx = idx + 1
+    return new_list
+            
