92f06e7c99ecbb06afbba5e6cf316ca1fe1a6658
Corbin <forstupidityonly@gmail.com>
2021-05-28 03:43:43 -0500

    lets see

---
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -0,0 +1,18 @@
+#!/usr/bin/node
+
+const request = require('request');
+request(process.argv[2], function (err, resp, bod) {
+    if (err) {
+      console.log(arr);
+    } else {
+      const todos = JSON.parse(bod);
+      const comptask = {};
+      todos.forEach((todo) => {
+      if (todo.completed && comptask[todo.userId] === undefined) {
+        comptask[todo.userId] = 1;
+      } else if (todo.completed) {
+        comptask[todo.userId] += 1;
+      }
+    });
+  }
+});
